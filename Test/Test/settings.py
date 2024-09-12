e8552e63ec5abbbebd6bb833d5647d8c95428c07
SebastianMT512 <sebastianmt512@gmail.com>
2024-09-12 02:42:43 -0500

    Deploy config

--- Test/Test/settings.py
+++ Test/Test/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'movies_api',
     'rest_framework',
+    'corsheaders',
 
 ]
 
@@ -50,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'Test.urls'
 
--- Test/movies_api/views.py
+++ Test/movies_api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework import status
 from rest_framework.response import Response
-from django.db.models import Count, Case, When, IntegerField,  IntegerField, FloatField, ExpressionWrapper,F
+from django.db.models import Count, Case, When, IntegerField,  IntegerField, FloatField, ExpressionWrapper,F,Q
 from .models import Movie
 from .serializer import MovieSerializer
 
@@ -20,7 +20,7 @@ class MoviesApiView(viewsets.ModelViewSet):
       movies =  self.get_Movie(id=id)
       if not movies:
           return Response(
-              {"res": "La pelicula no existe"}, status=status.HTTP_404_NOT_FOUND
+              {"res": "The element does not exist"}, status=status.HTTP_404_NOT_FOUND
           )
       serializer = MovieSerializer(movies, many=True)
       return Response(serializer.data, status = status.HTTP_200_OK)
@@ -41,7 +41,7 @@ class MoviesApiView(viewsets.ModelViewSet):
         Movies = self.get_Movie(Movie_id)
         if not Movies:
             return Response(
-                {"res": "La pelicula no existe"}, status=status.HTTP_404_NOT_FOUND
+                {"res": "The element does not exist"}, status=status.HTTP_404_NOT_FOUND
             )
         Movies = {
             "id": request.data.get("id"),
@@ -55,14 +55,14 @@ class MoviesApiView(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-   def delete(self, resquest, id, *args, **kwargs):
+   def delete(self, id, *args, **kwargs):
         movie = self.get_Movie(id)
         if not movie:
             return Response(
-                {"res": "El elemento no existe"}, status=status.HTTP_404_NOT_FOUND
+                {"res": "The element does not exist"}, status=status.HTTP_404_NOT_FOUND
             )
         movie.delete()
-        return Response({"res": "Elemento eliminado"}, status=status.HTTP_200_OK)
+        return Response({"res": "The element was deleted"}, status=status.HTTP_200_OK)
      
    @action(methods=['get'], detail=False, name='top')
    def top(self, request):
@@ -81,12 +81,31 @@ class MoviesApiView(viewsets.ModelViewSet):
             )
         ).values('rounded_rating').annotate(total=Count('id')).order_by('rounded_rating')
        rating_ranges = Movie.objects.aggregate(
-    rating1=Count(Case(When(rating__gte=1.0, rating__lt=1.9, then=1), output_field=IntegerField())),
-    rating2=Count(Case(When(rating__gte=2.0, rating__lt=2.9, then=1), output_field=IntegerField())),
-    rating3=Count(Case(When(rating__gte=3.0, rating__lt=3.9, then=1), output_field=IntegerField())),
-    rating4=Count(Case(When(rating__gte=4.0, rating__lt=4.9, then=1), output_field=IntegerField())),
-    rating5=Count(Case(When(rating__gte=5.0, then=1), output_field=IntegerField()))
-)
+        rating_1=Count(Case(When(rating__gte=1.0, rating__lt=1.9, then=1), output_field=IntegerField())),
+        rating_2=Count(Case(When(rating__gte=2.0, rating__lt=2.9, then=1), output_field=IntegerField())),
+        rating_3=Count(Case(When(rating__gte=3.0, rating__lt=3.9, then=1), output_field=IntegerField())),
+        rating_4=Count(Case(When(rating__gte=4.0, rating__lt=4.9, then=1), output_field=IntegerField())),
+        rating_5=Count(Case(When(rating__gte=5.0, then=1), output_field=IntegerField()))
+        )
        return Response({'movies_by_country': list(movies_country),
-            'movies_by_rating': list(movies_by_rating),
             'rating_ranges': rating_ranges}, status=status.HTTP_200_OK)
+       
+   @action(detail=False, methods=['get'], url_path='search')
+   def search(self, request):
+        query = request.query_params.get('q', None)
+        sort_column = request.query_params.get('s', None)  
+        sort_order = request.query_params.get('o', 'asc')
+        if query:
+            movies = Movie.objects.filter(Q(title__icontains=query) | Q(country__icontains=query))
+            if sort_column:
+                if sort_order == 'desc':
+                    sort_column = f'-{sort_column}'  
+                try:
+                    movies = movies.order_by(sort_column)
+                except Exception as e:
+                    return Response({"error": f"Invalid sort column: {sort_column}"}, status=status.HTTP_400_BAD_REQUEST)
+            serializer = MovieSerializer(movies, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Search query not provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
